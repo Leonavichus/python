d72d85d03ede5f54d260df6ff16c513306e9ad42
Yoshi <dleonavichus@mail.ru>
2022-09-14 14:17:26 +0300

    lab1

--- lab1/main.py
+++ lab1/main.py
@@ -1,13 +1,17 @@
+# Класс
 class Math:
+    # Конструктор
     def __init__(self,a,b,c,d):
         self.a = a
         self.b = b
         self.c = c
         self.d = d
 
+    # Возвращает строковое представление объекта
     def __str__(self):
         return f"Число a = {self.a}, число b = {self.b}, число c = {self.c}, число d = {self.d}"
 
+    # Деструктор
     def __del__(self):
         del self.a
         del self.b
@@ -15,21 +19,27 @@ class Math:
         del self.d
         print('Объект уничтожен')
 
+    # Функция для вычисления среднего арифмитического числа
     def get_arithmetic_mean(self):
         arithmetic_mean = (self.a+self.b+self.c+self.d) / 4
         return f"\nСреднее арифмитическое = {arithmetic_mean}"
 
+    # Функция для поиска максимального числа
     def get_maximum(self):
         maximum = max(self.a,self.b,self.c,self.d)
         return f"\nМаксимальное число = {maximum}"
 
+# Ввод переменных
 a  = float(input("Введите число a: "))
 b  = float(input("Введите число b: "))
 c  = float(input("Введите число c: "))
 d  = float(input("Введите число d: "))
 
+# Создание объекта
 math = Math(a,b,c,d)
 
+# Вывод данных
 print(math,math.get_arithmetic_mean(),math.get_maximum())
 
+# Уничтожение объекта
 del math
