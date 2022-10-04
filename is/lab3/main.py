510fd2822534fcdb4e36d1e3b2ab5c580047bda0
Yoshi <dleonavichus@mail.ru>
2022-10-04 18:25:40 +0300

    is lab3

---
+++ is/lab3/main.py
@@ -0,0 +1,22 @@
+import math
+import random
+import string
+
+P = pow(10, -5) # вероятность P подбора пароля злоумышленником
+V = 10 # паролей в минуту (скорость перебора паролей злоумышленником)
+T = 1 # неделя (срок действия)
+Vt = V*60*24*7 # паролей в неделю
+S1=(Vt*T)/P
+strPass = string.ascii_letters
+A = len(strPass)
+L = 6
+Al = pow(A, L)
+if(S1<Al):
+    print(f'Нижняя граница S* = {int(S1)}')
+else:
+    print('Сделать пароль длиннее')
+
+password =''
+for i in range(L):
+    password += random.choice(strPass)
+print(f'Пароль = {password}')
