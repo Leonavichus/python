197445611c07f777707acb2078a0ff111a0486f4
Yoshi <dleonavichus@mail.ru>
2022-10-28 01:25:34 +0300

    lab 3-5

--- lab4/main.py
+++ lab4/main.py
@@ -1,21 +1,152 @@
 from dataclasses import dataclass, asdict, astuple
+import pickle
+from collections import Counter
 
+# Классы
 @dataclass
-class Menu:
+class Post:
     id: int
     name: str
 
 @dataclass
-class Contant:
+class Teacher:
     id: int
-    name: str
-    description: str
-    id_author: int
-    id_menu: int
+    fcs: str
+    age: int
+    id_department: int
+    id_post: int
 
 @dataclass
-class Author:
+class Department:
     id: int
     name: str
-    password: str
-    mail: str
+    institute: str
+
+# Массивы для заполнения
+post = []
+teacher = []
+department = []
+id_dep = []
+depMas = []
+depCount = []
+ageMas = []
+ageCount = []
+
+# Заполенние массива
+def add(file, object):
+    with open(f"{file}.pickle", "rb") as f:
+        while True:
+            try:
+                o = pickle.load(f)
+            except EOFError:
+                break
+            else:
+                object.append(o)
+
+# Чтение из файла
+def load_object(filename):
+    with open(filename, "rb") as f:
+        while True:
+            try:
+                o = pickle.load(f)
+            except EOFError:
+                break
+            else:
+                print (o)
+
+# Создание объекта класса
+def create(number, obj, names):
+    with open(f"{names}.pickle", "ab") as f:
+        id = int(input('Id: '))
+        name = str(input('Название: '))
+        if(number == 2):
+            age = int(input('Возвраст: '))
+            id_department = int(input('Id кафедры: '))
+            id_post = int(input('Id должности: '))
+            pickle.dump(obj(id, name, age, id_department,id_post), f, protocol=pickle.HIGHEST_PROTOCOL)
+        elif(number == 3):
+            institute = str(input('Институт: '))
+            pickle.dump(obj(id, name, institute), f, protocol=pickle.HIGHEST_PROTOCOL)
+        else:
+            pickle.dump(obj(id, name), f, protocol=pickle.HIGHEST_PROTOCOL)
+
+add('teacher', teacher)
+add('department', department)
+add('post', post)
+exit = 0
+while(not exit):
+    start = int(input(f'Возможности:\n'
+    '1 - Вывести данные\n'
+    '2 - Создать объект\n'
+    '3 - Кафедра, на которой работает больше всего сотрудников\n'
+    '4 - Список кафедр в порядке убывания количества сотрудников\n'
+    '5 - Самая молодая кафедра\n'
+    '6 - Выход\n'))
+    if start == 1:
+        readObject = int(input(f'Вывести:\n'
+        '1 - Должности\n'
+        '2 - Преподаватели\n'
+        '3 - Кафедры\n'))
+        if readObject == 1:
+            load_object("post.pickle")
+        elif readObject == 2:
+            load_object("teacher.pickle")
+        elif readObject == 3:
+            load_object("department.pickle")
+    elif start == 2:
+        createObject = int(input(f'Создать:\n'
+        '1 - Должности\n'
+        '2 - Преподаватели\n'
+        '3 - Кафедры\n'))
+        if createObject == 1:
+            create(createObject, Post, 'post')
+        elif createObject == 2:
+            create(createObject, Teacher, 'teacher')
+        elif createObject == 3:
+            create(createObject, Department, 'department')
+    elif start == 3:
+        # Заполнение массива
+        for g in range(len(teacher)):
+            id_dep.append(teacher[g].id_department)
+        # Поиск числа, которое повторяется больше всего
+        maxId = max(a for a in id_dep if id_dep.count(a) == max(map(id_dep.count, id_dep)))
+        for element in department:
+            if(element.id == maxId):
+                print(f'Кафедра с наибольшим количчеством сотрудников: {element.name}')
+    elif start == 4:
+        # Заполенние массива
+        for g in range(len(teacher)):
+            depCount.append(teacher[g].id_department)
+        # Подсчет количества одинаковых элементов
+        a = dict(Counter(depCount))
+        # Заполенние массива
+        for g in range(len(department)):
+            depMas.append((department[g].id, department[g].name))
+        # Преобразование в словарь
+        myDict = {depMas[i][0]: depMas[i][1] for i in range(0, len(depMas), 1)}
+        dictNew = []
+        # Создание кортежа для сортировки (добавляем количество сотрудников и навзание кафедры)
+        for i in range(1, len(myDict)+1):
+            dictNew.append((myDict[i], a[i]))
+        # Сортировка
+        dictNew = sorted(dictNew, key=lambda tup: tup[1], reverse=True)
+        print(dictNew)
+    elif start == 5:
+        # Заполенние массивов
+        for g in range(len(teacher)):
+            ageMas.append((teacher[g].id_department, teacher[g].age))
+            ageCount.append(teacher[g].id_department)
+        # Подсчет одинаковых элементов
+        a = Counter(ageCount)
+        c = Counter()
+        # Создание словаря из списка складывая значения в одинаковых ключах
+        [c.update({k: int(v)}) for k,v in ageMas]
+        # Деление значений двух словарей
+        b = dict((k, float(c[k])/a[k]) for k in c)
+        # Получение минимального возраста
+        minId = min(b, key=b.get)
+        for element in department:
+            if(element.id == minId):
+                print(f'Самая молодая кафедра: {element.name}')
+    elif start == 6:
+        exit = 1
--- lab5/main.py
+++ lab5/main.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Функция подсчета y
 def z(x, a, b, c, d, f, g):
     a = a
     b = b
@@ -14,6 +15,7 @@ def z(x, a, b, c, d, f, g):
 
     return np.e * np.sin(a * x ** 2 + b * x + c) + d * np.cos(f * x + g)
 
+# Ввод данных
 num1 = float(input('a = '))
 num2 = float(input('b = '))
 num3 = float(input('c = '))
@@ -21,12 +23,13 @@ num4 = float(input('d = '))
 num5 = float(input('f = '))
 num6 = float(input('g = '))
 
+# Создание массива чисел x
 xx = np.arange(-10, 11, 1)
+# Подсчет массива чисел y
 yy = z(xx, num1, num2, num3, num4, num5, num6)
 
+# Построение и вывод диаграммы
 fig, ax = plt.subplots()
-
 ax.plot(xx, yy)
 ax.grid(linestyle=":")
-
 plt.show()
