68da4fedf8fe7f360dda0781a39a6cbc66eb572b
Yoshi <dleonavichus@mail.ru>
2022-11-02 16:53:31 +0300

    fix lab6

--- lab6/main.py
+++ lab6/main.py
@@ -1,26 +1,27 @@
 # Сортировка выбором
 import math
+import copy
+from random import randint
 
 # Сортировка векторов
 def selection_sort(myListOfVectors, number):
     list = []
     # Расчет длинны вектора
-    if number == 2:
-        for i in range(len(myListOfVectors)):
-            for j in range(len(myListOfVectors[i]) - 1):
-                list.append(math.sqrt(myListOfVectors[i][j] * myListOfVectors[i][j] + myListOfVectors[i][j+1] * myListOfVectors[i][j+1]))
-    elif number == 3:
-        for i in range(len(myListOfVectors)):
-            for j in range(len(myListOfVectors[i]) - 2):
-                list.append(math.sqrt(myListOfVectors[i][j] * myListOfVectors[i][j] + myListOfVectors[i][j+1] * myListOfVectors[i][j+1] + myListOfVectors[i][j+2] * myListOfVectors[i][j+2]))
+    # if number == 2:
+    #     for i in range(len(myListOfVectors)):
+    #         for j in range(len(myListOfVectors[i]) - 1):
+    #             list.append(math.sqrt(myListOfVectors[i][j] * myListOfVectors[i][j] + myListOfVectors[i][j+1] * myListOfVectors[i][j+1]))
+    # elif number == 3:
+    #     for i in range(len(myListOfVectors)):
+    #         for j in range(len(myListOfVectors[i]) - 2):
+    #             list.append(math.sqrt(myListOfVectors[i][j] * myListOfVectors[i][j] + myListOfVectors[i][j+1] * myListOfVectors[i][j+1] + myListOfVectors[i][j+2] * myListOfVectors[i][j+2]))
 
     # Сортировка выбором
-    for i in range(0, len(list) - 1):
+    for i in range(0, len(myListOfVectors) - 1):
         smallest = i
-        for j in range(i + 1, len(list)):
-            if list[j] < list[smallest]:
+        for j in range(i + 1, len(myListOfVectors)):
+            if myListOfVectors[j] < myListOfVectors[smallest]:
                 smallest = j
-        list[i], list[smallest] = list[smallest], list[i]
         myListOfVectors[i], myListOfVectors[smallest] = myListOfVectors[smallest], myListOfVectors[i]
 
 # Ввод данных
@@ -31,15 +32,21 @@ myListOfVectors = []
 # Создание списка векторов
 for i in range(1, n + 1):
     if category == 2:
-        x = int(input(f'Введите x{i} = '))
-        y = int(input(f'Введите y{i} = '))
+        x = randint(-10, 10)
+        y = randint(-10, 10)
         myListOfVectors.append((x,y))
     elif category == 3:
-        x = int(input(f'Введите x{i} = '))
-        y = int(input(f'Введите y{i} = '))
-        z = int(input(f'Введите z{i} = '))
+        x = randint(-10, 10)
+        y = randint(-10, 10)
+        z = randint(-10, 10)
         myListOfVectors.append((x,y,z))
 
 # Сортировка и вывод отсортированного списка векторов
-selection_sort(myListOfVectors, category)
+myListOfVectors2 = copy.copy(myListOfVectors)
 print(myListOfVectors)
+selection_sort(myListOfVectors, category)
+b = sorted(myListOfVectors2)
+print(f'Ручная сортировка = {myListOfVectors}')
+print(f'Сортировка библиотеки = {b}')
+if(b == myListOfVectors):
+    print('Совпадают!')
